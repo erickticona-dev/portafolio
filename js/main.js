3d32c150412552cd84a8078e1914c764281ac41a
Erick Ticona <erick.ticona.dev@gmail.com>
2023-04-09 16:50:34 -0500

    v3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+/* Typing Animation */
+var typed = new Typed(".typing",{
+    strings:["","Profesional en desarrollo web y diseñador UI/UX", "Diseñador Web","Diseñador Gráfico"],
+    typeSpeed:50,
+    BackSpeed:60,
+    loop:true
+})
--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* Typing Animation */
-var typed = new Typed(".typing",{
-    strings:["", "Web Designer","Web Developer","Graphic Designer","Youtuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
