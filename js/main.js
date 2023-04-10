53cfa1d2a74fee8a33d448766ce0b9ef1840ce79
Erick Ticona <erick.ticona.dev@gmail.com>
2023-04-10 01:00:40 -0500

    v4

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-/* Typing Animation */
-var typed = new Typed(".typing",{
-    strings:["","Profesional en desarrollo web y diseñador UI/UX", "Diseñador Web","Diseñador Gráfico"],
-    typeSpeed:50,
-    BackSpeed:60,
-    loop:true
-})
